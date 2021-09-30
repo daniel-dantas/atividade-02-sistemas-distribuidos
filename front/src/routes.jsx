9a4a8cdc060ba135db3d6748ba857aeab71fe004
Ruan-Miguel <miguelcoelho148@gmail.com>
2021-09-30 20:07:25 -0300

    remove home page

--- front/src/routes.jsx
+++ front/src/routes.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import Home from './pages/Home';
 import Pesquisa from './pages/Pesquisa';
 import RelatedArtists from './pages/RelatedArtists';
 import Artists from './pages/Artists';
 
-const Routes = ()=>{
-    return(
+const Routes = () => {
+    return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/">
+                    <Redirect to="/pesquisa" />
+                </Route>
                 <Route path="/pesquisa" component={Pesquisa} />
                 <Route path="/artistas/:name" component={Artists} />
                 <Route path="/artistasrelacionados/:id" component={RelatedArtists} />
